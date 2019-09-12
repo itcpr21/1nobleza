7f9decbff96f312200573eaba9de6c8d7e46f07b
nobleza <>
2019-09-12 13:05:13 +0700

    final

---
+++ Noblezadb.sql
@@ -0,0 +1,20 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.26-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+-- Dumping data for table noblezadb.noblezatbl: ~2 rows (approximately)
+/*!40000 ALTER TABLE `noblezatbl` DISABLE KEYS */;
+INSERT INTO `noblezatbl` (`ID`, `username`, `password`, `fname`, `lname`) VALUES
+	(1, 'kill', 'kill', 'kill', 'kill'),
+	(2, 'admin', 'admin', 'admin', 'admin');
+/*!40000 ALTER TABLE `noblezatbl` ENABLE KEYS */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ src/Configurator/Connection.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Configurator;
+
+/**
+ *
+ * @author user
+ */
+public class Connection {
+    public static String conUrl = "jdbc:mysql://localhost/noblezadb?user=root&password=";
+}
